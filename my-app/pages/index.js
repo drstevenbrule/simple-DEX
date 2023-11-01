6670ba5a3577b5f853b0fcb655733cc6158dfb1c
drstevenbrule <110744990+drstevenbrule@users.noreply.github.com>
2023-11-01 12:16:53 -0500

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -16,31 +16,6 @@ import {
 	removeLiquidity,
 } from "../utils/removeLiquidity";
 import { swapTokens, getAmountOfTokensReceivedFromSwap } from "../utils/swap";
-
-export default function Home() {
-	extends React.Component { 
-  	render() { 
-    const opts = { 
-      height: "390", 
-      width: "640", 
-      playerVars: { 
-        autoplay: 1, 
-      }, 
-    }; 
-  
-    return ( 
-      <div> 
-        <h3>GeeksforGeeks - Youtube</h3> 
-        <YouTube videoId="sTnm5jvjgjM" 
-            opts={opts} onReady={this._onReady} /> 
-      </div> 
-    ); 
-  } 
-  
-  _onReady(event) { 
-    event.target.pauseVideo(); 
-  } 
-}
 	const zero = BigNumber.from(0);
 	const [loading, setLoading] = useState(false);
 	const [liquidityTab, setLiquidityTab] = useState(false);
